f957b6878cea1467872509d1b8f3d51989a150a3
asror-0306 <luckyielts3366@gmail.com>
2025-02-27 18:54:09 +0500

    lesson-3 is done

---
+++ lesson-3/index.js
@@ -0,0 +1,130 @@
+"use strict"
+
+
+
+// if (0) {
+//     console.log("true");
+
+// } else {
+//     console.log("false");
+
+// }
+
+
+//////// SWITCH CASE
+
+
+/*
+const date = 5;
+switch (date) {
+    case 1:
+        console.log("Dushanba"); break;
+    case 2:
+        console.log("Seshanba"); break;
+    case 3:
+        console.log("Chorshanba"); break;
+    case 4:
+        console.log("Payshanba"); break;
+    default:
+        console.log("Error day");
+
+}
+*/
+
+
+
+///// Ternary Operator
+
+// const you = 50;
+// you == false ? console.log("men") : console.log("sen");
+
+
+
+/////// Logical operator
+
+// null && console.log("Okay");
+// null || console.log("Okay");
+
+
+///// Nullish Coleasing operator
+
+/*
+const state = 11;
+state ?? console.log("Success");
+
+const define = undefined;
+define ?? console.log("Success");
+*/
+
+
+
+//////////////////////// LOOPS
+
+
+////// for loop
+
+// DRY - Don't Repeat Yourself
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+
+// }
+
+// for (let m = 12; m <=100; m++) {
+//     console.log(m);
+
+// }
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+//     if (i % 2 == 0) break;
+
+// }
+
+///////////// While loop
+
+// let n = 1;
+// while (n < 10) {
+//     n++
+//     console.log(n);
+
+// }
+
+
+/////////////// Do while
+
+// let n = 0;
+
+// do {
+//     n++;
+//     console.log(n);
+
+// } while (n <= 10);
+
+
+////////////////////////// FUNCTION...
+
+/////// FUNCTION DECLARATION
+/////// FUNCTION ARROW
+/////// FUNCTION EXPRESSION
+
+
+
+/// FUNCTION DECLARATION
+
+
+
+function getName(a, d) {
+    console.log("Hello world");
+    console.log(a, d);
+
+}
+
+getName(1, 2)
+getName()
+getName()
+getName()
---
+++ lesson-3/reame.md
@@ -0,0 +1,9 @@
+### Scopes
+### Loops
+### For loop
+### while
+### do while
+### switch case
+### break keyword
+### continue keyword
+### Functions(declaration)
